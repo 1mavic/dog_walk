6e6a0b3c07e9226162c8d365fc4855e4a3f848ab
Aleksey Macegora <alekseymacegora@MacBook-Air-Aleksey.local>
2022-04-12 23:27:31 +0300

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:doggie_walker/environment.dart';
 import 'package:doggie_walker/flavor_config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -10,8 +13,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,45 +33,120 @@ class MapScreen extends StatefulWidget {
 }
 
 class MapScreenState extends State<MapScreen> {
-  final _controller = GoogleMapController.init;
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
+  final CameraPosition _kGooglePlex = const CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
+  final CameraPosition _kLake = const CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
+  final circle = Circle(
+      circleId: CircleId('first'),
+      center: LatLng(37.43296265331129, -122.08832357078792),
+      radius: 50,
+      fillColor: Colors.blueAccent,
+      visible: true,
+      strokeColor: Colors.blue,
+      consumeTapEvents: true,
+      onTap: () => log('tap'));
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: _DrawerWidget(),
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal, //satellite normal
+            initialCameraPosition: _kGooglePlex,
+            //myLocationButtonEnabled: false,
+            circles: {circle},
+            myLocationButtonEnabled: false,
+            myLocationEnabled: true,
+            onMapCreated: (GoogleMapController controller) {},
+          ),
+          _DrawerButtonWidget()
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(CupertinoIcons.location_circle),
+      ),
+    );
+  }
+}
+
+class _DrawerButtonWidget extends StatelessWidget {
+  const _DrawerButtonWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {},
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+    return Positioned(
+      top: 10,
+      right: 16,
+      child: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            Scaffold.of(context).openEndDrawer();
+          },
+          child: Container(
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor, shape: BoxShape.circle),
+            padding: const EdgeInsets.all(6),
+            child: const Icon(
+              CupertinoIcons.line_horizontal_3,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ),
       ),
     );
-    // @override
-    // Widget build(BuildContext context) {
-    //   return Scaffold(
-    //     body: SafeArea(
-    //         child: Center(
-    //       child: Text('sdsd'),
-    //     )),
-    //   );
   }
+}
 
-  Future<void> _goToTheLake() async {
-    // final GoogleMapController controller = await _controller;
-    // controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+class _DrawerWidget extends StatelessWidget {
+  const _DrawerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              DrawerHeader(
+                child: Text('header'),
+                duration: Duration(microseconds: 900),
+              )
+            ],
+          ),
+          Positioned(
+            top: 10,
+            right: 16,
+            child: SafeArea(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      shape: BoxShape.circle),
+                  padding: const EdgeInsets.all(6),
+                  child: const Icon(
+                    CupertinoIcons.clear,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
